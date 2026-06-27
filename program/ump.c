7d91d6a11482efde2ef9b010f3fbb6012a47fa61
ljxlljjxx <ljxlljjxxs@163.com>
2026-06-27 23:31:02 +0800

    fix some bug

--- program/ump.c
+++ program/ump.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define PATH "/Users/ljx/Desktop/P/github/ump"
 
 FILE *file;
 char str[10000];
+char new_str[10000] = "cd "PATH"; ";
 
 int main()
 {
-	file = fopen("/Users/ljx/useful_mini_program/ReadMe.txt", "r");
+	file = fopen(PATH"/ReadMe.txt", "r");
 	while (fgets(str, 10000, file) != NULL)
 	{
 		printf("%s", str);
 		if (str[0] == 'g' && str[1] == 'c' && str[2] == 'c')
 		{
-			system(str);
+			strcpy(new_str + 36, str);
+			system(new_str);
 		}
 	}
 	fclose(file);
